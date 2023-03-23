ef57841403652a1fec9c3dc7d2c9b4cdd63f7560
myatatotal <fabiorobertoferreira@gmail.com>
2023-03-23 08:46:53 -0300

    atualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ChartsComponent } from './charts/charts.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DatePipe } from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChartsComponent,
     
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    HttpClientJsonpModule
+    HttpClientJsonpModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
